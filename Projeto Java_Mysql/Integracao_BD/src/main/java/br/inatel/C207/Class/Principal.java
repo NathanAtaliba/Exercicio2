ff272920addf2b72d3926ed33af9a2cbb45633d1
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-06-12 22:26:49 -0300

    Projeto_MySql_Java

--- Projeto Java_Mysql/Integracao_BD/src/main/java/br/inatel/C207/Class/Principal.java
+++ Projeto Java_Mysql/Integracao_BD/src/main/java/br/inatel/C207/Class/Principal.java
@@ -17,7 +17,7 @@ MedicoDB m1 = new MedicoDB(); // Instanciando um medico pra atualizar o nome do 
         Cadastro cadastro1 = new Cadastro();  //Instanciando um paciente para tabela
         cadastro.insertCadastro(cadastro1);  //Inserindo um cadastro no banco de dados
             Atendente atendente1 = new Atendente("nathan",10,1,"Setor");
-           // at1.insertAtendente(atendente1); //Inserindo um novo atendente
+            at1.insertAtendente(atendente1); //Inserindo um novo atendente
                 Paciente paciente1 = new Paciente("Renan","Dor",20,1, atendente1.getId(), cadastro1.getIdCadastro());//Criando o primeiro paciente
         Cadastro cadastro2 = new Cadastro(); //Criando um novo cadastro
         cadastro.insertCadastro(cadastro1); //Inserindo novo cadastro
@@ -26,10 +26,10 @@ Funcionarios medico1 = new Medico("Nathan",20,2,"Medico"); //Criando o primeiro 
 cadastro.insertCadastro(cadastro2); //Inserindo um novo cadastroat1.updateAtendente("renan",10,1,"Medico");
         at1.researchAtendentes();
         at1.insertMedico(medico1); //Inserindo um novo medico no banco de dados
-            //  at1.insertPaciente(paciente1); //Inserindo um novo paciente no banco de dados
-            //  at1.insertPaciente(paciente2); //Inserindo um novo paciente no banco de dados
-      // at1.updateAtendente(atendente1);
-            at1.deleteAtendente(atendente1);
+             at1.insertPaciente(paciente1); //Inserindo um novo paciente no banco de dados
+              at1.insertPaciente(paciente2); //Inserindo um novo paciente no banco de dados
+     //  at1.updateAtendente(atendente1);
+       //   at1.deleteAtendente(atendente1);
             ///BUSCA DOS DADOS NO INTELIJEI
         at1.researchPacientes(); //Mostrando os pacientes via terminal
         at1.researchAtendentes();  //Mostrando os atendentes via terminal
